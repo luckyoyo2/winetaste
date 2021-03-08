647c9c45e252f70a31f62ffd0ac95e33de343acd
luckyoyo2 <yoann.diep@hotmail.fr>
2021-03-09 01:18:53 +0200

    Add the Robe html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let btn = document.querySelector('button');
 
-let e = document.addEventListener('click', btn => alert('Hey'))
+let e = document.addEventListener('click', btn => window.location.href = './robe.html');
