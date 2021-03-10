66e3cdbb6a2fe04a07cd4cf23a9ef3814dd05353
luckyoyo2 <yoann.diep@hotmail.fr>
2021-03-10 18:40:38 +0200

    Add a few styles in css

--- script.js
+++
@@ -1,3 +0,0 @@
-let btn = document.querySelector('button');
-
-let e = document.addEventListener('click', btn => window.location.href = './robe.html');
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    window.location.href = 'robe.html';
+});
